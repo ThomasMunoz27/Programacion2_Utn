b7929934c1ed5254b684afb804621ac930c5349b
ThomasMunoz27 <thomasnm2004@gmail.com>
2024-03-22 00:40:32 -0300

    Continuando ejercicio de cuentas bancarias(Creo q terminé)

--- Clase2/Ejercicio2/SavingAccount.java
+++ Clase2/Ejercicio2/SavingAccount.java
@@ -3,6 +3,10 @@ package Clase2.Ejercicio2;
 public class SavingAccount extends Account{
     protected boolean able;
 
+    public SavingAccount(float balance, float annualRate) {
+        super(balance, annualRate);
+    }
+
     public SavingAccount(float balance, float annualRate, boolean able) {
         super(balance, annualRate);
         if (this.balance < 10000){
@@ -15,14 +19,47 @@ public class SavingAccount extends Account{
         return able;
     }
 
-    public void consign(int dep){
-        if (able == true){
-            this.balance -= dep;
-            System.out.println("Se han consignado $" + dep);
-        }else {
-            System.out.println("No fue posible consignar, debido a que su cuenta está inactiva");
+    @Override
+    public void consign(float amount){
+        this.balance += amount;
+        numberConsignments++;
+            System.out.println("Se han consignado $" + amount);
+        if (balance >= 10000){
+            able = true;
+            System.out.println("Se ha activado su cuenta");
         }
 
     }
-
+    @Override
+    public void withdraw(float amount) {
+        float newBalance = balance - amount;
+        if (able) {
+            if (newBalance >= 0) {
+                balance -= amount;
+                numberWithdrawals++;
+                if (balance < 10000){
+                    able = false;
+                    System.out.println("Su cuenta de ahorro se ha desactivado");
+                }
+            } else {
+                System.out.println("La cantidad a retirar excede el saldo actual.");
+            }
+        }else {
+            System.out.println("No fue posible retirar dinero, debido a que su cuenta está inactiva");
+        }
+    }
+    @Override
+    public void monthlyStatement(){
+        if (numberWithdrawals > 4){
+            balance -= (numberWithdrawals - 4) *1000;
+        }
+        if (balance < 10000){
+            able = false;
+            System.out.println("Su cuenta de ahorro se ha desactivado");
+        }
+    }
+    public void print(){
+        int totalTransactions = numberConsignments + numberWithdrawals;
+        System.out.println("Saldo: " + this.balance + ".\nComision mensual: " + this.monthlyCommission + ".\nTransacciones realizadas: " + totalTransactions);
+    }
 }
