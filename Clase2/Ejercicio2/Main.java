13e6380db2cc47560db77304a77de72dda9ee940
ThomasMunoz27 <thomasnm2004@gmail.com>
2024-04-15 16:20:18 -0300

    cambios

--- Clase2/Ejercicio2/Main.java
+++ Clase2/Ejercicio2/Main.java
@@ -3,12 +3,15 @@ package Clase2.Ejercicio2;
 public class Main {
     public static void main(String[] args) {
         CurrentAccount ca1 = new CurrentAccount(1000, 50);
-        ca1.calculateInterest();
-        ca1.monthlyStatement();
-        ca1.withdraw(1500);
-        ca1.print();
-        ca1.consign(2000);
-        ca1.print();
-        ca1.print();
+        SavingAccount sa1 = new SavingAccount(22000,10);
+
+        sa1.consign(2000);
+        sa1.withdraw(5000);
+        sa1.consign(2000);
+        sa1.withdraw(5000);
+        sa1.consign(2000);
+        sa1.withdraw(5000);
+        sa1.monthlyStatement();
+        sa1.print();
     }
 }
